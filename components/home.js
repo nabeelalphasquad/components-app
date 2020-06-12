89afbf90343285d33bdc5736ea4b3a735257182d
nabeelalphasquad <nabeel.alphasquad@gmail.com>
2020-06-12 14:32:39 +0500

    styling and responsive

--- components/home.js
+++ components/home.js
@@ -10,73 +10,45 @@ const Home = () => {
 
   return (
 
-    <div className="App">
-      <div className="">
-        <div className="flex">
-          <h1>Marketing</h1>
-          <ul style={{ marginLeft: "100px" }}>
-            {
-              obj1 && obj1.map((item, index) => {
-                return (
-                  <div>
-                    <img src="../static/img-block.png" />
-                    <li key={index}> <a href={"/detail/" + item.name}><strong> {item.name}</strong> <br></br> {item.children.length} components </a></li>
-                  </div>
-                )
-              })
-            }
-          </ul>
-        </div>
-        <hr></hr>
-        <div className="flex margin-top">
-          <h1>Application-UI</h1>
-          <ul style={{ marginLeft: "100px" }}>
-            {
-              obj2 && obj2.map((item, index) => {
-                return (
-                  <div>
-                    <img src="../static/img-block.png" />
-                    <li key={index}> <a href={"/detail/" + item.name}> <strong>{item.name}</strong> <br></br>({item.children.length} components) </a> </li>
-                  </div>
-                )
-              })
-            }
-          </ul>
-        </div>
-        <hr></hr>
+    <div className="p-5">
+      <div className="mb-10 flex flex-col xl:flex-row lg:flex-row md:flex-col sm:flex-col">
+        <h1 className="w-full xl:w-1/4 lg:w-1/4 md:w-full sm:w-full text-3xl text-center p-10">Marketing</h1>
+        <ul className="w-full xl:w-3/4 lg:w-3/4 md:w-full sm:w-full flex flex-wrap justify-center">
+          {
+            obj1 && obj1.map((item, index) => {
+              return (
+                <div className="m-2">
+                  <a className="hover:no-underline hover:text-black" href={"/detail/" + item.name}>
+                  <img src="../static/img-block.png" />
+                  <li key={index}> <strong> {item.name}</strong> <br></br> {item.children.length} components </li>
+                  </a>
+                </div>
+              )
+            })
+          }
+        </ul>
       </div>
-      <style jsx>{`
-        .App {
-          text-align: left;
-          margin: 30px;
-        }
-        .flex{
-        justify-content: space-around;
-        display: flex;
-        }
-        .margin-top{
-          margin-top: 50px;
-        }
-        ul{
-          padding: 0px;
-          display: flex;
-          flex-wrap: wrap;
-        }
-        li{
-          display: flex;
-          list-style-type: none;
-          margin-bottom: 20px;
-          margin: 20px;
-        }
-        a{
-          text-decoration: none;
-          color: black;
-        }
-        img{
-          width:350px;
-        }
-        
-      `}</style>
+      <hr></hr>
+      <div className="flex flex-col xl:flex-row lg:flex-row md:flex-col sm:flex-col mt-10 mb-10">
+      <h1 className="w-full xl:w-1/4 lg:w-1/4 md:w-full sm:w-full text-3xl text-center p-10">Application UI</h1>
+        <ul className="w-full xl:w-3/4 lg:w-3/4 md:w-full sm:w-full flex flex-wrap justify-center">
+          {
+            obj2 && obj2.map((item, index) => {
+              return (
+                <div className="m-2">
+                  <a className="hover:no-underline hover:text-black" href={"/detail/" + item.name}>
+                  <img src="../static/img-block.png" />
+                  <li key={index}> <strong> {item.name}</strong> <br></br> {item.children.length} components </li>
+                  </a>
+                </div>
+              )
+            })
+          }
+        </ul>
+      </div>
+      <hr></hr>
+
+
     </div>
   );
 }
---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '@atlaskit/css-reset';
+// import "../styles/style.module.css";
+
+import '../style.css';
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
