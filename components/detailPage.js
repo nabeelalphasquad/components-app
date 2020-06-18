33382e6d164dddc84397b7136a4a65f320cab5ac
nabeelalphasquad <nabeel.alphasquad@gmail.com>
2020-06-18 15:40:01 +0500

    updates

--- components/detailPage.js
+++ components/detailPage.js
@@ -5,31 +5,29 @@ export default (props) => {
 
     let { data } = props
 
-    // const [main, setMain] = useState(null)
-    // useEffect(() => {
-    //     let copyData = data && { ...data }
-    //     copyData && copyData.children.forEach(sin => {
-    //         let path = sin.path.replace("../dh-components-source", "https://a.tuk.dev")
-    //         axios.get(path).then(res => {
-    //             sin.htmlData = res.data
-    //         })
-    //     })
-    //     setMain(copyData)
-    // }, [data])
+    const [main, setMain] = useState(null)
+    useEffect(() => {
+        let copyData = data && { ...data }
+        copyData && copyData.children.forEach(sin => {
+            let path = sin.path.replace("../dh-components-source", "https://a.tuk.dev")
+            axios.get(path).then(res => {
+                sin.htmlData = res.data
+            })
+        })
+        setMain(copyData)
+    }, [data])
     // console.log(main)
     return (
         <div>
             {
-                data && <div>
+                main && <div>
                     <ul>
                         {
-                            data.children.map((item, index) => {
+                            main.children.map((item, index) => {
                                 // console.log(item)
-                                let path = item.path.replace("../dh-components-source", "https://a.tuk.dev")
-
                                 return (
-                                    <li key={index} className="m-5">
-                                        <iframe src = {path} width="100%" height="100%"></iframe>
+                                    <li key={index} className="m-5"> 
+                                        <div dangerouslySetInnerHTML={{ __html: item.name && item.name }}></div>
                                     </li>
                                 )
                             })
